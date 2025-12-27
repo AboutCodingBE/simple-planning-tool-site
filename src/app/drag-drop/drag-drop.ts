2a275da390ed9f2a4ef896ddde2522f81b93e556
Nicholas Ocket <nicholas@aboutcoding.be>
2025-12-27 19:10:38 +0100

    make basic drag and drop planning possible

--- src/app/drag-drop/drag-drop.ts
+++ src/app/drag-drop/drag-drop.ts
@@ -15,16 +15,16 @@ export class DragDrop {
 
   drop(event: CdkDragDrop<string[]>) {
     console.log(event);
-      if (event.previousContainer === event.container) {
-        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-      } else {
-        transferArrayItem(
-          event.previousContainer.data,
-          event.container.data,
-          event.previousIndex,
-          event.currentIndex,
-        );
-      }
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
     }
+  }
 
 }
--- src/app/planning/planning-overview.ts
+++ src/app/planning/planning-overview.ts
@@ -1,11 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Planning, Week, UnplannedSite } from './domain'
+import { Planning, Week, UnplannedSite, SiteView, Day } from './domain';
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem,} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'planning-overview',
   standalone: true,
-  imports: [],
+  imports: [CdkDropList, CdkDrag ],
   templateUrl: './planning.html',
   styleUrl: './planning.css',
 })
@@ -13,7 +14,7 @@ export class PlanningOverview {
   private client = inject(HttpClient);
 
   planningWeeks = signal<Week[]>([]);
-  unplannedSites = signal<UnplannedSite[]>([])
+  unplannedSites = signal<SiteView[]>([])
 
   ngOnInit() {
     this.getDefaultPlanning();
@@ -30,9 +31,49 @@ export class PlanningOverview {
   getUnplannedSites() {
     this.client.get<UnplannedSite[]>('http://localhost:8080/sites/unplanned')
       .subscribe(result => {
-        console.log(result);
-        this.unplannedSites.set(result);
+        const siteViews: SiteView[] = result.map(site => ({
+          id: site.id,
+          name: site.name,
+          duration_in_days: site.duration_in_days,
+          status: 'OPEN'
+        }));
+        this.unplannedSites.set(siteViews);
     });
+  }
+
+  private getDaysFromWeek(week: Week): Day[] {
+      return [
+        week.monday,
+        week.tuesday,
+        week.wednesday,
+        week.thursday,
+        week.friday,
+        week.saturday,
+        week.sunday
+      ];
+    }
+
+    getConnectedLists(): string[] {
+      const allDayIds = this.planningWeeks()
+        .flatMap(week => this.getDaysFromWeek(week))
+        .map(day => day.date);
+
+      return ['unplanned', ...allDayIds];
+    }
+
+  drop(event: CdkDragDrop<SiteView[]>) {
+    console.log(event);
+    console.log(event.container.element.nativeElement.id)
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
