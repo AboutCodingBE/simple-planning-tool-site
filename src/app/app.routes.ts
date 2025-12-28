66816a298099deaf26d575e87322c249816671e9
Nicholas Ocket <nicholas@aboutcoding.be>
2025-12-28 11:20:49 +0100

    deleted test components, adjusted some css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,6 @@ import { SiteManagement } from './site-management/site-management'
 import { OpenSiteOverview } from './site-management/open-site-overview/open-site-overview'
 import { CreateSite } from './site-management/create-site/create-site'
 import { ChangeSite } from './site-management/change-site/change-site'
-import { Innerroutertest } from './innerroutertest/innerroutertest'
-import { Inner1 } from './innerroutertest/inner1/inner1'
-import { Inner2 } from './innerroutertest/inner2/inner2'
-import { DragDrop } from './drag-drop/drag-drop'
 
 export const routes: Routes = [
   {path: '', redirectTo: '/planning-overview', pathMatch: 'full'},
@@ -19,11 +15,4 @@ export const routes: Routes = [
         { path: 'createSite', component: CreateSite },
         { path: 'changeSite/:id', component: ChangeSite },
       ]},
-  {path: 'innerroutertest', component: Innerroutertest,
-    children: [
-      { path: '', redirectTo: 'inner1', pathMatch: 'full' },
-      { path: 'inner1', component: Inner1 },
-      { path: 'inner2', component: Inner2 },
-      ]},
-    {path: 'dragndrop', component: DragDrop}
  ];
